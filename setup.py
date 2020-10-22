28f9a1c219d48c805f41b075324f840155f0acbe
Karina-Tamara Delgado Moscoso <69841810+KarinaTDM@users.noreply.github.com>
2020-10-22 19:19:32 +0200

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name='atsocial',
-      packages=['atsocial'],
-      include_package_data=True,
-      install_requires=[
-          'flask',
-      ],
-      )
