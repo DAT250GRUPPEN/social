b90ee44a5207aedeafc96b814baf89a9785cbe39
Skole <Skole@Ingrids-MacBook-Pro.local>
2020-10-23 11:58:06 +0200

    endret i setup

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
-from setuptools import setup
+# from setuptools import setup
 
-setup(name='atsocial',
-      packages=['atsocial'],
-      include_package_data=True,
-      install_requires=[
-          'flask',
-      ],
-      )
+# setup(name='atsocial',
+#       packages=['atsocial'],
+#       include_package_data=True,
+#       install_requires=[
+#           'flask',
+#       ],
+#       )
+
+from atsocial import app
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
