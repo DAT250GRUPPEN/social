20cafde7537f9a2523f22b8b83fc3085f3f77881
Karina-Tamara Delgado Moscoso <karinatdm@stud.uis.no>
2020-10-23 11:16:40 +0200

    Ny commit

---
+++ atsocial/views.py
@@ -0,0 +1,28 @@
+import datetime
+from flask import render_template
+from atsocial import app
+
+pages = [{'id': 1, 'title': 'Home','figure_name':'fa fa-home'},\
+        {'id': 2, 'title': 'Upload','figure_name':'fa fa-cloud-upload'},\
+        {'id':3, 'title':'Friends', 'figure_name': 'fa fa-users'},\
+        {'id':4, 'title': 'Myprofile','figure_name': 'fa fa-id-badge'}]
+
+ekstrapages = [{'id': 1, 'title':'Settings', 'figure_name':'fa fa-cogs'},\
+        {'id':2, 'title':'Help/FAQ', 'figure_name':'fa fa-question-circle-o'},\
+        {'id': 3, 'title':'Log Out', 'figure_name':'fa fa-lock'}]
+
+# dictionary
+
+@app.route('/')
+def index():
+    return render_template('index.html', pages=pages) 
+
+@app.route('/page/<int:page_id>')
+def page(page_id):
+    return render_template('page.html', page=pages[page_id - 1])
+
+@app.route('/ekstrapage/<int:ekstrapage_id>')
+def ekstrapage(ekstrapage_id):
+    return render_template('ekstrapage.html', ekstrapage=ekstrapages[ekstrapage_id - 1])
+
+
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='atsocial',
+      packages=['atsocial'],
+      include_package_data=True,
+      install_requires=[
+          'flask',
+      ],
+      )
